7e4185abb6d3046b918b7d619c7340b056224a9b
0x1772 <realhateux@outlook.com>
2023-05-29 23:05:55 +0300

    starting_programs added, chat_daily updated

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import pyttsx3
 from gtts import gTTS
 from pygame import mixer
 import speech_recognition as sr
+import pyaudio
+import psutil
 
 mode = "text"
 voice = "pyttsx"
@@ -17,7 +19,7 @@ terminate = ["bye", "quit", "by", "bb", "goodbye", "good bye", "see you later", 
 def get_arguments():
     parser = argparse.ArgumentParser()
     optional = parser.add_argument_group("paramets")
-    optional.add_argument("-v", "--voice", action="store_true", required=False, help="enable voice")
+    optional.add_argument("-v", "--voice", action="store_true", required=False, help="Enable Voice Mode")
     optional.add_argument("-g", "--googlespeech", action="store_true", required=False, help="enable googlespeech")
     arguments = parser.parse_args()
     return arguments
@@ -33,6 +35,18 @@ def google_speak(synaptic_speak):
     mixer.music.play()
     while mixer.music.get_busy():
         time.sleep(1)
+        
+# Firefox açan kod        
+def open_firefox():
+    firefox_path = r"C:\Program Files\Firefox Developer Edition\firefox.exe"
+    webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path), 1)
+    webbrowser.get('firefox').open('about:blank')
+    print("Firefox Developer Edition uygulaması açıldı.")
+
+# Bilgisayar sıcaklık değerini alma
+def get_temperature():
+    temperature = psutil.sensors_temperatures()['coretemp'][0].current
+    return str(temperature)
 
         
 def offline_speech(synaptic_speak):
@@ -49,16 +63,19 @@ def online_speech(synaptic_speak):
 def listen():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Dinleniyor...")
         audio = r.listen(source)
     try:
-        print(r.recognize_google(audio))
-        return r.recognize_google(audio)
+        print(r.recognize_google(audio, language="tr"))
+        return r.recognize_google(audio, language="tr")
     except sr.UnknownValueError:
         online_speech("Ne dediğinizi anlayamadım, lütfen tekrar eder misiniz?")
         return listen()
     except sr.RequestError as e:
         print("Veri alınamıyor: Google Speech Recognition servisi; {0}".format(e))
+        online_speech("Veri alınamıyor, lütfen tekrar deneyin.")
+        return listen()
+
 
 if __name__ == "__main__":
     args = get_arguments()
