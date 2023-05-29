1b58049bc664234ded39216cce2830e8bf616883
0x1772 <realhateux@outlook.com>
2023-05-29 19:20:47 +0300

    google_speak_update speeding added

--- main.py
+++ main.py
@@ -4,39 +4,52 @@ import os
 import time
 import argparse
 import pyttsx3
+from gtts import gTTS
+from pygame import mixer
+import speech_recognition as sr
 
 mode = "text"
 voice = "pyttsx"
-terminate = ["bye", "quit","by","bb","goodbye","good bye","see you later","seeyoulater","off",]
+terminate = ["bye", "quit", "by", "bb", "goodbye", "good bye", "see you later", "seeyoulater", "off"]
 
 # GET ARGUMENTS FROM USER
 def get_arguments():
     parser = argparse.ArgumentParser()
     optional = parser.add_argument_group("paramets")
-    optional.add_argument("-v", "--voice", action = "store_true", required = False, help = "enable voice")
-    optional.add_argument("-g", "--googlespeech", action = "store_true", required = False, help = "enable googlespeech")
+    optional.add_argument("-v", "--voice", action="store_true", required=False, help="enable voice")
+    optional.add_argument("-g", "--googlespeech", action="store_true", required=False, help="enable googlespeech")
     arguments = parser.parse_args()
     return arguments
 
-def google_speak(synaptic_speak):
-    tts = gTTS(text = synaptic_speak, lang = "tr")
+def google_speak(synaptic_speak, speed=1.0):
+    tts = gTTS(text=synaptic_speak, lang="tr")
     tts.save("audio.mp3")
+
+    sound = AudioSegment.from_file("audio.mp3")
+    modified_sound = sound.speedup(playback_speed=speed)
+    modified_sound.export("audio.mp3", format="mp3")
+
     mixer.init()
     mixer.music.load("audio.mp3")
     mixer.music.play()
-    while mixer.music.get_busy():
-        time.sleep(2)
 
-def offline_speech(synaptic_speak):
+    while mixer.music.get_busy():
+        time.sleep(1)
+
+def offline_speak(synaptic_speak):
     engine = pyttsx3.init()
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Konuşma hızı (isteğe bağlı)
+    engine.setProperty('volume', 0.8)  # Ses seviyesi (isteğe bağlı)
+    engine.setProperty('voice', 'tr')  # Türkçe seslendirme için 'tr' kullanılır
     engine.say(synaptic_speak)
     engine.runAndWait()
-    
+
 def online_speech(synaptic_speak):
-    if(voice == "gTTS"):
-        gtts_speak(synaptic_speak)
+    if voice == "gTTS":
+        google_speak(synaptic_speak)
     else:
-        offline_speech(synaptic_speak)
+        offline_speak(synaptic_speak)
 
 def listen():
     r = sr.Recognizer()
@@ -44,52 +57,41 @@ def listen():
         print("Listening...")
         audio = r.listen(source)
     try:
-        speech = r.recognize_google(audio)
-        print("You said: " + speech)
-        return speech
+        print(r.recognize_google(audio))
+        return r.recognize_google(audio)
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-        return 0
+        online_speech("Ne dediğinizi anlayamadım, lütfen tekrar eder misiniz?")
+        return listen()
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        return 0
-    
+        print("Veri alınamıyor: Google Speech Recognition servisi; {0}".format(e))
+
 if __name__ == "__main__":
     args = get_arguments()
-    if(args.voice):
+    if args.voice:
         try:
-            import speech_recognition as sr
             mode = "voice"
-        
         except ImportError:
-            print("\nInstall speech_recognition to use this feature" + "\nStarting text mode")
-    
-    if(args.googlespeech):
+            print("\nspeech_recognition'ı kullanmak için yükleyin" + "\nMetin moduyla başlatılıyor.")
+
+    if args.googlespeech:
         try:
-            from gtts import gTTS
-            from pygame import mixer
             voice = "gTTS"
-        
         except ImportError:
-            import pyttsx
-            print("\n Please install pyttsx to use this feature" + "\nUseing pyttsx ")
-    
-    else:
-        import pyttsx3
-        
+            print("\nLütfen 'pyttsx3'ü yükleyin" + "\npyttsx3 kullanılıyor.")
+
     kernel = aiml.Kernel()
     if os.path.isfile("bot_brain.brn"):
-        kernel.bootstrap(brainFile = "bot_brain.brn")
+        kernel.bootstrap(brainFile="bot_brain.brn")
     else:
-        kernel.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B") 
-    
+        kernel.bootstrap(learnFiles="std-startup.xml", commands="LOAD AIML B")
+
     while True:
         if mode == "voice":
             response = listen()
         else:
-            response = input("Enter The Message:  ")
-        if response.lower().replace(" ", "") in terminate: 
+            response = input("Enter The Message: ")
+        if response.lower().replace(" ", "") in terminate:
             break
         synaptic_speak = kernel.respond(response)
-        print("synaptic: " + synaptic_speak )
-        google_speak(synaptic_speak)
+        print("Synaptic: " + synaptic_speak)
+        online_speech(synaptic_speak)
