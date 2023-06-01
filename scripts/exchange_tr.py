54ddf2aeb6189fd9cc3209717543bfe948988657
0x1772 <realhateux@outlook.com>
2023-06-01 09:34:37 +0300

    exchange and webdriver update

---
+++ scripts/exchange_tr.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+
+def para_birimi_fiyati(sembol):
+    url = f"https://api.exchangerate-api.com/v4/latest/TRY"
+    response = requests.get(url)
+    data = response.json()
+
+    if "rates" in data:
+        return data["rates"].get(sembol)
+    else:
+        return None
+
+if len(sys.argv) < 2:
+    print("Lütfen bir para birimi sembolü girin.")
+    sys.exit(1)
+
+sembol = sys.argv[1]
+fiyat = para_birimi_fiyati(sembol)
+
+if fiyat:
+    tl_fiyat = 1 / fiyat  # Girilen para biriminin TL karşılığı
+    print(f"1 {sembol} = {tl_fiyat} TL")
+else:
+    print("Geçerli bir para birimi sembolü girmediniz veya fiyat bulunamadı.")
