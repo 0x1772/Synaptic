dc402b242377550663d4866d05ccbbadfadfcfba
0x1772 <realhateux@outlook.com>
2023-06-14 16:44:41 +0300

    change log_directory path in main.py

--- windows_installation.ps1
+++ windows_installation.ps1
@@ -16,4 +16,22 @@ $output = "$file.tmp"
 Move-Item -Path $output -Destination $file -Force
 
 Write-Host "Dosya güncellendi: $file"
-Pause
+
+$directory = Join-Path -Path $PSScriptRoot -ChildPath "log_directory"
+New-Item -ItemType Directory -Path $directory -Force | Out-Null
+# Kullanıcıdan log dizinini al
+$logDirectory = Read-Host -Prompt "Log dizinini girin (örn: C:\logs):"
+
+# main.py dosyasının yolunu al
+$scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
+
+# main.py dosyasını oku
+$content = Get-Content -Path "$scriptPath\main.py"
+
+# Yeni log dizinini kullanarak main.py dosyasını güncelle
+$content = $content -replace 'log_directory = ".+"', "log_directory = '$logDirectory'"
+
+# Güncellenmiş içeriği main.py dosyasına yaz
+Set-Content -Path "$scriptPath\main.py" -Value $content
+
+Write-Host "main.py dosyası güncellendi."
