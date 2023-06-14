7b567de7de008faae2fe12c5b82aee871da2514a
0x1772 <realhateux@outlook.com>
2023-06-14 16:25:45 +0300

    some updates (not stable)

--- gui.py
+++ gui.py
@@ -3,23 +3,29 @@ import dash_fluentui_components as dfui
 
 app = dash.Dash(__name__)
 
-app.layout = dfui.FluentLayout(
-    body=dfui.FluentListView(
-        items=[
-            dfui.FluentListTile(
-                title="Package 1",
-                onTap=dfui.FluentCallback(lambda: print("Installing Package 1")),
-            ),
-            dfui.FluentListTile(
-                title="Package 2",
-                onTap=dfui.FluentCallback(lambda: print("Installing Package 2")),
-            ),
-        ],
-    ),
-    floatingActionButton=dfui.FluentFloatingActionButton(
-        child=dfui.FluentIcon(dfui.FluentIcons.add),
-        onPressed=dfui.FluentCallback(lambda: print("Adding Package")),
-    ),
+app.layout = dfui.Card(
+    children=[
+        dfui.CardBody(
+            children=[
+                dfui.FluentListTile(
+                    title="Package 1",
+                    onTap=dfui.FluentCallback(lambda: print("Installing Package 1")),
+                ),
+                dfui.FluentListTile(
+                    title="Package 2",
+                    onTap=dfui.FluentCallback(lambda: print("Installing Package 2")),
+                ),
+            ],
+        ),
+        dfui.CardFooter(
+            children=[
+                dfui.FluentFloatingActionButton(
+                    child=dfui.FluentIcon(dfui.FluentIcons.add),
+                    onPressed=dfui.FluentCallback(lambda: print("Adding Package")),
+                ),
+            ],
+        ),
+    ],
 )
 
 if __name__ == "__main__":
